9ed4a4a691fc6d42b3def576c2e68ee0362c2d38
Alex <alex@Alexs-Mac-2.local>
2013-08-12 19:32:31 -0300

    t commit>:w

---
+++ ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  
+//
+//  Created by Alex on 4/7/13.
+//  Copyright (c) 2013 AlexSem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
